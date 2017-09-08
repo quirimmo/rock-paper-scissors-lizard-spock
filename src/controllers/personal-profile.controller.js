f442cc92d291c056c503210c1f60f0ed4ef1a7fc
quirimmo <quirimmo@gmail.com>
2017-09-08 23:35:16 +0100

    started implementing the code of the personal profile page

--- src/controllers/personal-profile.controller.js
+++ src/controllers/personal-profile.controller.js
@@ -6,10 +6,20 @@
     function PersonalProfileController() {
 
         var vm = this;
+
+        // using the reveal patter defining at the beginning all the used variables inside the view
+        vm.numOfMatches;
+        vm.numOfVictories;
+        vm.numOfLoses;
+        vm.numOfDraws;
+        vm.consecutiveNumOfVictories;
+        vm.consecutiveNumOfLoses;
+        vm.consecutiveNumOfDraws;
+
         vm.$onInit = onInit;
 
         function onInit() {
-            console.log('Init of personal profile controller');
+            
         }
 
     }
