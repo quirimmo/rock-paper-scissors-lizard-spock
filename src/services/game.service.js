3e652cc1a098d34282dd4729b3d13a78f986e26f
quirimmo <quirimmo@gmail.com>
2017-09-09 00:11:12 +0100

    implemented the code of the game service

--- src/services/game.service.js
+++ src/services/game.service.js
@@ -3,17 +3,86 @@
 
     angular.module('myApp').service('gameService', distanceService);
 
-    function distanceService() {
+    function distanceService(sessionStorage) {
 
         // list of the exposed methods
         // ============================================================
+        
+        this.getNumOfMatches = getNumOfMatches;
+        this.getNumOfVictories = getNumOfVictories;
+        this.getNumOfLoses = getNumOfLoses;
+        this.getNumOfDraws = getNumOfDraws;
+        this.getNumOfConsecutiveVictories = getNumOfConsecutiveVictories;
+        this.getNumOfConsecutiveLoses = getNumOfConsecutiveLoses;
+        this.getNumOfConsecutiveDraws = getNumOfConsecutiveDraws;
 
+        this.incrementNumOfMatches = incrementNumOfMatches;
+        this.incrementNumOfVictories = incrementNumOfVictories;
+        this.incrementNumOfLoses = incrementNumOfLoses;
+        this.incrementNumOfDraws = incrementNumOfDraws;
+        this.incrementNumOfConsecutiveVictories = incrementNumOfConsecutiveVictories;
+        this.incrementNumOfConsecutiveLoses = incrementNumOfConsecutiveLoses;
+        this.incrementNumOfConsecutiveDraws = incrementNumOfConsecutiveDraws;
 
 
         // list of the methods implementations
         // ============================================================
 
-        
+        function getNumOfMatches() {
+            return sessionStorage.numOfMatches;
+        }
+
+        function getNumOfVictories() {
+            return sessionStorage.numOfVictories;
+        }
+
+        function getNumOfLoses() {
+            return sessionStorage.numOfLoses;
+        }
+
+        function getNumOfDraws() {
+            return sessionStorage.numOfDraws;
+        }
+
+        function getNumOfConsecutiveVictories() {
+            return sessionStorage.numOfConsecutiveVictories;
+        }
+
+        function getNumOfConsecutiveLoses() {
+            return sessionStorage.numOfConsecutiveLoses;
+        }
+
+        function getNumOfConsecutiveDraws() {
+            return sessionStorage.numOfConsecutiveDraws;
+        }
+
+        function incrementNumOfMatches() {
+            sessionStorage.numOfMatches++;
+        }
+
+        function incrementNumOfVictories() {
+            sessionStorage.numOfVictories++;
+        }
+
+        function incrementNumOfLoses() {
+            sessionStorage.numOfLoses++;
+        }
+
+        function incrementNumOfDraws() {
+            sessionStorage.numOfDraws++;
+        }
+
+        function incrementNumOfConsecutiveVictories() {
+            sessionStorage.numOfConsecutiveVictories++;
+        }
+
+        function incrementNumOfConsecutiveLoses() {
+            sessionStorage.numOfConsecutiveLoses++;
+        }
+
+        function incrementNumOfConsecutiveDraws() {
+            sessionStorage.numOfConsecutiveDraws++;
+        }
 
     }
 })();
