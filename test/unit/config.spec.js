df9fa3359bbd89e4d3fa61eb9155c97638fbe79b
quirimmo <quirimmo@gmail.com>
2017-09-08 19:39:13 +0100

    fixing the already defined unit tests of config

--- test/unit/config.spec.js
+++ test/unit/config.spec.js
@@ -1,15 +1,17 @@
-describe('config.js', function() {
+fdescribe('config.js', function() {
 
-    let $state, $rootScope;
+    let $state, $rootScope, $httpBackend;
 
     beforeEach(function() {
         module('myApp');
 
         module(function($provide) {});
 
-        inject(function(_$state_, _$rootScope_) {
+        inject(function(_$state_, _$rootScope_, _$httpBackend_) {
             $state = _$state_;
             $rootScope = _$rootScope_;
+            $httpBackend = _$httpBackend_;
+            $httpBackend.whenGET(/(.*)/).respond(200, 'aaaaaaa');
         });
 
     });
@@ -18,17 +20,20 @@ describe('config.js', function() {
 
         it('should load the URL', function() {
             expect($state.href('home')).toEqual('#!/');
+            $httpBackend.flush();
         });
 
         it('should go the state', function() {
             $state.go('home');
             $rootScope.$apply();
+            $httpBackend.flush();
             expect($state.current.name).toEqual('home');
         });
 
         it('should return the URL as default if wrong url has been provided', function() {
             $state.go('nomatchedurl');
             $rootScope.$apply();
+            $httpBackend.flush();
             expect($state.current.url).toEqual('/');
         });
 
@@ -37,13 +42,13 @@ describe('config.js', function() {
     describe('Rock Paper Scissors State', function() {
 
         it('should load the URL', function() {
-            $state.go('rock-paper-scissors');
             expect($state.href('rock-paper-scissors')).toEqual('#!/rock-paper-scissors');
         });
 
-        it('should go the home state', function() {
+        it('should go the rock paper scissors state', function() {
             $state.go('rock-paper-scissors');
             $rootScope.$apply();
+            $httpBackend.flush();
             expect($state.current.name).toEqual('rock-paper-scissors');
         });
 
@@ -52,13 +57,13 @@ describe('config.js', function() {
     describe('Rock Paper Scissors Lizard Spock State', function() {
 
         it('should load the URL', function() {
-            $state.go('rock-paper-scissors-lizard-spock');
             expect($state.href('rock-paper-scissors-lizard-spock')).toEqual('#!/rock-paper-scissors-lizard-spock');
         });
 
         it('should go the home state', function() {
             $state.go('rock-paper-scissors-lizard-spock');
             $rootScope.$apply();
+            $httpBackend.flush();
             expect($state.current.name).toEqual('rock-paper-scissors-lizard-spock');
         });
 
