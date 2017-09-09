8179f65aee40f21966cd5469bd5f8746dd5e5818
quirimmo <quirimmo@gmail.com>
2017-09-09 14:38:38 +0100

    defined main structure of the service

---
+++ src/services/game-engine.service.js
@@ -0,0 +1,17 @@
+(function() {
+    'use strict';
+
+    angular.module('myApp').service('gameEngineService', gameEngineService);
+
+    function gameEngineService() {
+
+        // list of the exposed methods
+        // ============================================================
+
+
+        // list of the methods implementations
+        // ============================================================
+
+        
+    }
+})();
