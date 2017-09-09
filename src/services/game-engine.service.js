73ebbcee41e877f6e1aa3499ad89733556f1ea93
quirimmo <quirimmo@gmail.com>
2017-09-09 15:01:27 +0100

    defined exposed functions in the service

--- src/services/game-engine.service.js
+++ src/services/game-engine.service.js
@@ -7,11 +7,39 @@
 
         // list of the exposed methods
         // ============================================================
-
+        this.getRockPaperScissorsSubset = getRockPaperScissorsSubset; 
+        this.getRockPaperScissorsLizardSpockSubset = getRockPaperScissorsLizardSpockSubset; 
+        this.getRockPaperScissorsLizardSpockChuckSubset = getRockPaperScissorsLizardSpockChuckSubset; 
+        this.calculateResult = calculateResult; 
+        this.getWinText = getWinText; 
+        this.getLoseText = getLoseText; 
 
         // list of the methods implementations
         // ============================================================
 
+        function getRockPaperScissorsSubset() {
+
+        }
+
+        function getRockPaperScissorsLizardSpockSubset() {
+            
+        }
+        
+        function getRockPaperScissorsLizardSpockChuckSubset() {
+            
+        }
+
+        function calculateResult() {
+            
+        }
+
+        function getWinText() {
+            
+        }
+
+        function getLoseText() {
+            
+        }
         
     }
 })();
