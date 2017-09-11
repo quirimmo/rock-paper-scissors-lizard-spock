92d7e5aa64fbb56c66e907d48d637723c5a7bd74
quirimmo <quirimmo@gmail.com>
2017-09-11 11:04:20 +0100

    refactored and commented all the main menu component

--- src/components/main-menu/main-menu.controller.js
+++ src/components/main-menu/main-menu.controller.js
@@ -22,6 +22,8 @@
         // ==============================================================
 
         function onInit() {
+            // init the current selected item of the menu based on the page you are actually in
+            // lot of people hate ternary operator nested, but I actually love it when the operations you are going to do are simple assignments based on conditions
             vm.selectedItem =
                 vm.isRockPaperScissorActive() ?
                 'rock-paper-scissors' :
@@ -32,30 +34,63 @@
                 'personal-profile';
         }
 
+        /**
+         * A function used in order to know if you are in the Personal Profile Page. 
+         * It is just for readability. I love to have boolean conditions expressed through functions in order to improve the readability of the code
+         * It makes easier from reading the code where they are used, what you are expecting from that check simply reading the name of the functions, 
+         * avoiding to focus on the expression they are checking
+         * @returns {Boolean} True if the current page is the Personal Profile page 
+         */
         function isPersonalProfileActive() {
             return vm.activeItem === 'Personal Profile';
         }
 
+        /**
+         * A function used in order to know if you are in the Rock Paper Scissors Page. 
+         * @returns {Boolean} True if the current page is the Rock Paper Scissors page 
+         */
         function isRockPaperScissorActive() {
             return vm.activeItem === 'Rock Paper Scissors';
         }
 
+        /**
+         * A function used in order to know if you are in the Rock Paper Scissors Lizard Spock Page. 
+         * @returns {Boolean} True if the current page is the Rock Paper Scissors Lizard Spock page 
+         */
         function isRockPaperScissorLizardSpockActive() {
             return vm.activeItem === 'Rock Paper Scissors Lizard Spock';
         }
 
+        /**
+         * A function used in order to know if you are in the Rock Paper Scissors Lizard Spock Chuck Norris Page. 
+         * @returns {Boolean} True if the current page is the Rock Paper Scissors Lizard Spock Chuck Norris page 
+         */
         function isRockPaperScissorLizardSpockChuckActive() {
             return vm.activeItem === 'Rock Paper Scissors Lizard Spock Chuck Norris';
         }
 
+        /**
+         * Navigate to the page corresponding to the provided state
+         * @param {String} state The name of the state where you want to go  
+         */
         function navigateTo(state) {
             $state.go(state);
         }
 
+        /**
+         * Open the mobile menu
+         * @param {Object} $mdMenu Instance of the $mdMenu component of Angular Material
+         * @param {Object} ev The firing event 
+         */
         function openMobileMenu($mdMenu, ev) {
             $mdMenu.open(ev);
         }
 
+        /**
+         * Action performed when you click on the Restart button of the menu
+         * It shows a confirmation dialog and if confirmed, it calls the gameService.restartGame method in order to perform a restart
+         * @param {Object} ev The firing event 
+         */
         function restartGame(ev) {
             gameService;
             var confirm = $mdDialog.confirm()
