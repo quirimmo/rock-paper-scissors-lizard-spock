344f5df4870972374a83eb64cbe3d4b114beb7c1
quirimmo <quirimmo@gmail.com>
2017-09-11 11:48:08 +0100

    commented all the controllers

--- src/controllers/rock-paper-scissors-lizard-spock.controller.js
+++ src/controllers/rock-paper-scissors-lizard-spock.controller.js
@@ -1,3 +1,9 @@
+/**
+ * Controller associated to the Rock Paper Scissors Lizard Spock page
+ * It is responsible for assigning two main parameters:
+ *  - gameTitle {String} The name of the game to be generated
+ *  - availableChoices {Array} An array of the actions to be used for generating this current game instance
+ */
 (function() {
     'use strict';
 
@@ -10,13 +16,15 @@
         // using the reveal patter defining at the beginning all the used variables inside the view
         vm.gameTitle;
         vm.availableChoices;
-        vm.$onInit = onInit;
 
+        vm.$onInit = onInit;
 
         // ==========================================================
 
         function onInit() {
+            // set the game title
             vm.gameTitle = 'Rock Paper Scissors Lizard Spock';
+            // retrieve the available choices for this game passing through the gameEngineService.getRockPaperScissorsLizardSpockSubset method
             vm.availableChoices = gameEngineService.getRockPaperScissorsLizardSpockSubset();
         }
 
