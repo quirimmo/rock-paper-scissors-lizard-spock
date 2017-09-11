cbfcc6af1d5b24ac65d15fb7aa611f4830777576
quirimmo <quirimmo@gmail.com>
2017-09-11 11:30:07 +0100

    commented the game constants

--- src/constants/game.constants.js
+++ src/constants/game.constants.js
@@ -1,3 +1,27 @@
+/**
+ * GAME CONSTANTS
+ * An object which holds all the available actions you inside all your games
+ * An action (so an available choice for the game) is an object composed by:
+ * - id {String} The id of the current action. It is used for identifying the action. It is also used for retrieving the action icon. In the assets/images folder you provide an icon called with "id.png" name convention, and this will be automatically associated to this action
+ * - label {String} The label to be shown for the action, when displayed in the choices or used when showing the result of the match
+ * - winsAgainst {Array} An array of objects of all the actions that will loose against the current action. A winsAgainst object is so composed:
+ *      - id: The id of the loosing action
+ *      - term: The verb specifying how the current action wins against this element
+ * - losesAgainst {Array} An array of objects of all the actions that will win against the current action. A losesAgainst object is so composed:
+ *      - id: The id of the winning action
+ *      - term: The verb specifying how the current actions loses against this element. The passive "has been ... by" will be automatically added by the system
+ * 
+ * As assumption, all the actions will draw against themselves. 
+ * You could change this behavior adding a new drawsAgainst Array, but it actually makes totally no sense that an action doesn't draw against itself
+ * 
+ * At the moment 6 actions are supported:
+ *  - Rock
+ *  - Paper
+ *  - Scissors
+ *  - Lizard
+ *  - Spock
+ *  - Chuck Norris 
+ */
 (function() {
     'use strict';
 
